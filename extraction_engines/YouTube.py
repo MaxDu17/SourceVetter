137eaf19c221775c975fdde12ff24cfe7f73d245
MaxDu <maximilianjdu@gmail.com>
2022-10-31 01:15:49 -0700

    Update YouTube.py

--- extraction_engines/YouTube.py
+++ extraction_engines/YouTube.py
@@ -7,23 +7,41 @@ import tqdm
 from Translator import get_keywords
 
 with open("../secrets/youtube_api.txt") as f:
-    API_KEY = f.readline() #"AIzaSyATjYPjE31HbOJYDZgjcIQmapaA7EtIoBc"
+    API_KEY = f.readline()
 
-CHANNELID = "UC2nhCnIGssNalfnHgcaglBQ"
-request = f"https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={CHANNELID}&part=snippet,id&order=date&maxResults=20"
+class YouTube(Translator):
+    def __init__(self, relevant_words, channel_id, max_results = 20):
+        self.channel_id = channel_id
+        self.max_results = max_results
+        self.search_query = f"https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={self.channel_id}&part=snippet,id&order=date&maxResults={self.max_results}"
+        super().__init__(relevant_words)
+
+    def grab_relevant_links(self):
+        page = requests.get(url=self.search_query)
+        videos = json.loads(page.text)["items"]
+
+        for video in videos:
+            try:
+                video_id = video["id"]["videoId"]  # sometimes you don't hit a video and you hit a playlist for some reason
+            except:
+                continue
+            publish_time = video["snippet"]["publishedAt"]
+            title = video["snippet"]["title"]
+            print(title)
+            for word in self.relevant_words:
+                if title is not None and word in title:
+
+            if url is not None and self.relevant_prefix in url:
+                for word in self.relevant_words:
+                    if title is not None and word in title:
+                        link_dict[title] = url
+                        break
+
+# CHANNELID = "UC2nhCnIGssNalfnHgcaglBQ"
+# request = f"https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={CHANNELID}&part=snippet,id&order=date&maxResults=20"
 
 
-page = requests.get(url = request)
-videos = json.loads(page.text)["items"]
 
-for video in videos:
-    try:
-        video_id = video["id"]["videoId"] #sometimes you don't hit a video and you hit a playlist for some reason
-    except:
-        continue
-    publish_time = video["snippet"]["publishedAt"]
-    title = video["snippet"]["title"]
-    print(title)
 # items -> list of videos -> id/videoId, snipped/publishedAt, title,
 
 import ipdb
